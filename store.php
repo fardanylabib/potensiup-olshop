a9b15a5b395540add1a809b761fd34106390546a
Muhammad S Maromi <mmaromi@alamisharia.co.id>
2022-08-08 06:58:56 +0700

    Remove grid/list view selection

--- store.php
+++ store.php
@@ -109,10 +109,6 @@ include 'header.php';
 									</select>
 								</label>
 							</div>
-							<ul class="store-grid">
-								<li class="active"><i class="fa fa-th"></i></li>
-								<li><a href="#"><i class="fa fa-th-list"></i></a></li>
-							</ul>
 						</div>
 						<!-- /store top filter -->
 
